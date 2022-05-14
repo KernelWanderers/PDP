a6c16b229873eb4eb23756b24457916854f04330
kernelBPD <twitchisadeck@gmail.com>
2022-05-14 19:56:24 +0200

    fix(GVP): simplify

--- python/src/linux/gvp.py
+++ python/src/linux/gvp.py
@@ -1,10 +1,8 @@
 def get_valid_pcip(slot):
     try:
-        slot, func = [
+        return tuple([
             hex(int(n, 16)) for n in
             slot.split(':')[-1].split('.')
-        ]
+        ])
     except Exception:
         return (None, None)
-
-    return (slot, func)
