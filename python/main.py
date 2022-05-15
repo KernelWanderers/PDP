5c10b5ba61e408652b96250b4b780c6ab66c1427
= <=>
2022-05-15 22:05:06 +0530

    Include support for Python 3.8, add fallback phrase when ACPI path not found

--- python/main.py
+++ python/main.py
@@ -3,39 +3,42 @@ if __name__ == '__main__':
     from src.util.format import format_data_linux
 
     print()
-    match platform.system().lower():
-        case 'linux':
-            from src.linux.find_pci_devices import find_pci_devices
-            from src.linux.construct_pcip import construct_pcip
 
-            _format_data = []
-            for dev in find_pci_devices():
-                data = construct_pcip(dev)
+    if platform.system().lower() == "linux":
+        from src.linux.find_pci_devices import find_pci_devices
+        from src.linux.construct_pcip import construct_pcip
 
-                _format_data.append(data)
-
-            print(*format_data_linux(_format_data))
+        _format_data = []
         
-        case 'darwin':
-            from src.osx.find_pci_devices import find_pci_devices
-            from src.osx.construct_pcip import construct_pcip
-            from src.osx.pcip_from_plane import pcip_from_plane
-            from src.util.format import format_data_osx
+        for dev in find_pci_devices():
+            data = construct_pcip(dev)
+            _format_data.append(data)
 
-            _format_data = []
+        print(*format_data_linux(_format_data))
+    
+    elif platform.system().lower() == "darwin":
+        from src.osx.find_pci_devices import find_pci_devices
+        from src.osx.construct_pcip import construct_pcip
+        from src.osx.pcip_from_plane import pcip_from_plane
+        from src.util.format import format_data_osx
 
-            for dev in find_pci_devices():
-                data = construct_pcip(dev.get('Device'))
-                acpi_plane, pci = pcip_from_plane(dev.get('Plane'))
+        _format_data = []
+        for dev in find_pci_devices():
+            data = construct_pcip(dev.get('Device'))
+            acpi_plane, pci = pcip_from_plane(dev.get('Plane'))
 
-                if not data:
-                    data = pci
+            if not data:
+                data = pci
+            
+            _format_data.append({
+                "PCI-Debug": dev.get("PCI-Debug"),
+                "Dev-Ven": dev.get("Dev-Ven"),
+                "ACPI-Plane": acpi_plane,
+                "PCI-Path": pci
+            })
+        
+        print(*format_data_osx(_format_data))
 
-                _format_data.append({
-                    'PCI-Debug': dev.get('PCI-Debug'),
-                    'Dev-Ven': dev.get('Dev-Ven'),
-                    'ACPI-Plane': acpi_plane,
-                    'PCI-Path': pci
-                })
-
-            print(*format_data_osx(_format_data))
+    elif platform.system().lower() == "windows":
+        print("Windows support coming soon!")
+        exit(0)
--- python/src/linux/find_pci_devices.py
+++ python/src/linux/find_pci_devices.py
@@ -11,13 +11,15 @@ def find_pci_devices():
     for path in os.listdir(pci_dev_path):
         slot, func = get_valid_pcip(path)
 
-        if not os.path.isdir(os.path.join(pci_dev_path, path, 'firmware_node')):
-            continue
+        if os.path.isdir(os.path.join(pci_dev_path, path, 'firmware_node')):
+            acpi_path = open(os.path.join(pci_dev_path, path, 'firmware_node', 'path'), 'r').read().replace('\n', '')
+        else:
+            acpi_path = "ACPI_Path_Not_Found"
 
         devices.append({
             'path': os.path.join(pci_dev_path, path),
             'components': (slot, func),
-            'acpi': open(os.path.join(pci_dev_path, path, 'firmware_node', 'path'), 'r').read().replace('\n', '')
+            'acpi': acpi_path
         })
         
     return devices
